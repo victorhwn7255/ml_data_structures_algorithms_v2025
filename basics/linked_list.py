1717568b92d1d6dd5f473563671882dfcb71759b
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2025-09-17 15:16:01 +0800

    - linked list

--- basics/linked_list.py
+++ basics/linked_list.py
@@ -1,1 +1,25 @@
-#test
+class Node:
+  def __init__(self, value):
+    self.value = value
+    self.next = None
+
+class LinkedList:
+  def __init__(self, value):
+    new_node = Node(value)
+    self.head = new_node
+    self.tail = new_node
+    self.length = 1
+    
+  #def append(self, value):
+    
+  
+  #def prepend(self, value):
+  
+  
+  #def insert(self, index, value):
+  
+  
+
+myLinkedList = LinkedList(4)
+
+print(myLinkedList.tail.value)
