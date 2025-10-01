9d5a43a7b34c91797eb0a86cbeacde4680dd0301
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2025-10-01 17:32:27 +0800

    - BFS and DFS

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -48,6 +48,48 @@ class BinarySearchTree:
       else:
         return True
     
+  def BFS(self):
+    current_node: Node = self.root
+    queue = []
+    results = []
+    
+    queue.append(current_node)
+    while len(queue) > 0:
+      current_node = queue.pop(0)
+      results.append(current_node.value)
+      if current_node.left is not None:
+        queue.append(current_node.left)
+      if current_node.right is not None:
+        queue.append(current_node.right)
+    
+    return results
+    
+  def DFS_Pre_Order(self):
+    results = []
+    
+    def traverse(current_node: Node):
+      results.append(current_node.value)
+      if current_node.left is not None:
+        traverse(current_node.left)
+      if current_node.right is not None:
+        traverse(current_node.right)
+      
+    traverse(self.root)
+    return results
+  
+  def DFS_Post_Order(self):
+    results = []
+    
+    def traverse(current_node: Node):
+      if current_node.left is not None:
+        traverse(current_node.left)
+      if current_node.right is not None:
+        traverse(current_node.right)
+      results.append(current_node.value)
+      
+    traverse(self.root)
+    return results
+      
     
 my_BST = BinarySearchTree()
 
@@ -55,10 +97,8 @@ my_BST.insert(3)
 my_BST.insert(1)
 my_BST.insert(6)
 
-
-
 print(my_BST.root.value)
 print(my_BST.root.left.value)
 print(my_BST.root.right.value)
 
-print(my_BST.contains(9))
+print(my_BST.BFS())
