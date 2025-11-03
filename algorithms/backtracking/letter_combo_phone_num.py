8f782058547b35af024abb31847438b3725f60c1
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2025-11-04 07:13:17 +0800

    - letter combo phone number

--- algorithms/backtracking/letter_combo_phone_num.py
+++ algorithms/backtracking/letter_combo_phone_num.py
@@ -1,9 +1,32 @@
 def letterCombinations(digits: str) -> list[str]:
+  digit_to_letters = {
+      '2': 'abc',
+      '3': 'def',
+      '4': 'ghi',
+      '5': 'jkl',
+      '6': 'mno',
+      '7': 'pqrs',
+      '8': 'tuv',
+      '9': 'wxyz'
+  }
+  results = []
   
+  if not digits:
+    return []
   
+  def backtrack(index, path: list):
+    if index == len(digits):
+      results.append("".join(path))
+      return
+    
+    for char in digit_to_letters[digits[index]]:
+      path.append(char)
+      backtrack(index + 1, path)
+      path.pop()
   
+  backtrack(0, [])
   
-  return digits
+  return results
 
 test_input_1 = "23"
 test_input_2 = "2"
