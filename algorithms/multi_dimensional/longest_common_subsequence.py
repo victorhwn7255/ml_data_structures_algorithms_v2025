e839b8046d5ab87cec8bf3db322d8ba0f87e40ca
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2025-11-11 07:54:37 +0800

    - longest common subsequence

--- algorithms/multi_dimensional/longest_common_subsequence.py
+++ algorithms/multi_dimensional/longest_common_subsequence.py
@@ -1,13 +1,20 @@
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+
 from utils.visualize_db_table import visualize_dp
 
 def longestCommonSubsequence(text1, text2):
   dp = [[0 for _ in range(len(text2) + 1)] for _ in range(len(text1) + 1)]
-  visualize_dp(dp, text1, text2)
   
-  return text1
+  for i in range(1, len(text1) + 1):
+    for j in range(1, len(text2) + 1):
+      if text1[i-1] == text2[j-1]:
+        dp[i][j] = dp[i-1][j-1] + 1
+      else:
+        dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+  
+  return dp[i][j]
 
 print(longestCommonSubsequence("abcde", "ace"))
 print(longestCommonSubsequence("abc", "abc"))
