e4aa287b933132c2d7047bad3149830c663dc96a
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2025-12-22 08:39:33 +0800

    - repeat container with most water

---
+++ algorithms/two_pointers/R_container_with_most_water.py
@@ -0,0 +1,17 @@
+def maxArea(height: list[int]):
+  max_area = 0
+  left = 0
+  right = len(height) - 1
+  
+  while left < right:
+    current_area = min(height[left], height[right]) * (right - left)
+    max_area = max(current_area, max_area)
+    if height[left] < height[right]:
+      left += 1
+    else:
+      right -= 1
+  
+  return max_area
+
+print(maxArea([1,8,6,2,5,4,8,3,7]))
+print(maxArea([1,1]))
---
+++ algorithms/two_pointers/index_first_occurrence_in_string.py
@@ -0,0 +1,7 @@
+def strStr(haystack: str, needle: str):
+  
+  
+  return needle
+
+print(strStr("sadbutsad", "sad"))
+print(strStr("leetcode", "leeto"))
