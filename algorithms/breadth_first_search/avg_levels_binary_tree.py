3a72816b61fa5a2eb4174cf2b278c46a95d4152f
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-01-16 06:52:20 +0800

    - avg levels binary tree

---
+++ algorithms/breadth_first_search/avg_levels_binary_tree.py
@@ -0,0 +1,40 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from utils.tree_utils import print_tree, build_tree
+
+class TreeNode:
+   def __init__(self, val=0, left=None, right=None):
+      self.val = val
+      self.left = left
+      self.right = right
+   
+   def __str__(self):
+      return f"TreeNode({self.val})"
+   
+   def print_tree(self):
+      print_tree(self)
+      
+root_test_1 = build_tree([3,9,20,None,None,15,7], TreeNode)
+root_test_2 = build_tree([3,9,20,15,7], TreeNode)
+
+from collections import deque
+def averageOfLevels(root: TreeNode):
+  queue = deque([root])
+  result = []
+  while queue:
+    level_size = len(queue)
+    current_sum = 0
+    for _ in range(level_size):
+      node = queue.popleft()
+      current_sum += node.val
+      if node.left:
+        queue.append(node.left)
+      if node.right:
+        queue.append(node.right)
+    result.append(current_sum/level_size)
+  
+  return result
+
+print(averageOfLevels(root_test_1))
+print(averageOfLevels(root_test_2))
