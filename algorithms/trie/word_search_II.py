2eb313be73264b26fcef9a524c58bc96edd0c1fa
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-01-31 07:05:02 +0800

    - word search II

---
+++ algorithms/trie/word_search_II.py
@@ -0,0 +1,22 @@
+def findWords(board: list[list[str]], words: list[str]):
+  
+  
+  
+  return words
+
+
+test_board_1 = [
+  ["o","a","a","n"],
+  ["e","t","a","e"],
+  ["i","h","k","r"],
+  ["i","f","l","v"]
+]
+test_words_1 = ["oath","pea","eat","rain"]
+test_board_2 = [
+  ["a","b"],
+  ["c","d"]
+]
+test_words_2 = ["abcb"]
+
+print(findWords(test_board_1, test_words_1))
+print(findWords(test_board_2, test_words_2))
