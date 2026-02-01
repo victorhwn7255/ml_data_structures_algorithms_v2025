9e6d0561a6925c4748f82e32398454fed0065acf
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-02-01 09:08:21 +0800

    - word search II

--- algorithms/trie/word_search_II.py
+++ algorithms/trie/word_search_II.py
@@ -1,8 +1,48 @@
+class TrieNode():
+  def __init__(self):
+    self.children = {}
+    self.word = None
+
 def findWords(board: list[list[str]], words: list[str]):
+  rows = len(board)
+  cols = len(board[0])
+  root = TrieNode()
+  for word in words:
+    node = root
+    for char in word:
+      if char not in node.children:
+        node.children[char] = TrieNode()
+      node = node.children[char]
+    node.word = word
   
+  results = []
+  directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+  def dfs(r, c, node: TrieNode):
+    char = board[r][c]
+    if char not in node.children:
+      return
+    
+    next_node: TrieNode = node.children[char]
+    if next_node.word is not None:
+      results.append(next_node.word)
+      next_node.word = None
+    
+    board[r][c] = '#'
+    for (dr, dc) in directions:
+      nr, nc = r+dr, c+dc
+      if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] != '#':
+        dfs(nr, nc, next_node)
+    board[r][c] = char
+    
+    if not next_node.children and next_node.word is None:
+      del node.children[char]
+    
+  for r in range(rows):
+    for c in range(cols):
+      if board[r][c] in root.children:
+        dfs(r, c, root)
   
-  
-  return words
+  return results
 
 
 test_board_1 = [
