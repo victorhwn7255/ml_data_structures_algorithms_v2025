d35be38e74b2806395e86984fdf04cd89bffc42f
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-02-11 07:23:35 +0800

    - generate parentheses

--- algorithms/backtracking/generate_parentheses.py
+++ algorithms/backtracking/generate_parentheses.py
@@ -3,24 +3,22 @@ def generateParenthesis(n):
   left_count = 1
   path = ['(']
   
-  def backtrack(left_count, path:list):
+  def backtrack(open_count, left_count, path:list):
     if len(path) == n*2:
       results.append(''.join(path))
       return
     
     if left_count > 0:
       path.append(')')
-      backtrack(left_count-1, path)
+      backtrack(open_count, left_count-1, path)
       path.pop()
-
-    open_count = (len(path) + left_count) // 2
     
     if open_count < n:
       path.append('(')
-      backtrack(left_count+1, path)
+      backtrack(open_count+1, left_count+1, path)
       path.pop()
   
-  backtrack(left_count, path)
+  backtrack(1, left_count, path)
   return results
 
 print(generateParenthesis(3))
