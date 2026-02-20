70b3fb8c179f54181b15669b65036c9a65bfca4c
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-02-21 07:11:11 +0800

    - find peak element

---
+++ algorithms/binary_search/R_find_peak_element.py
@@ -0,0 +1,17 @@
+def findPeakElement(nums: list[int]):
+  left = 0
+  right = len(nums)-1
+  
+  while left < right:
+    mid = (left + right) // 2
+
+    if nums[mid] < nums[mid + 1]:
+      left = mid + 1
+    else:
+      right = mid  
+    
+  return left
+
+
+print(findPeakElement([1,2,3,1]))
+print(findPeakElement([1,2,1,3,5,6,4]))
